6d18be837927a0a62347aa5c16e95e4bc4871ee4
Paul Mathieu <56612315+paul-mathieu@users.noreply.github.com>
2021-09-29 14:31:44 +0200

    few changes

--- convert.py
+++ convert.py
@@ -7,7 +7,7 @@ content_loc = [{loc: e[1]} for e in content for loc in e[0]]
 content_gen = [{gen: e[0]} for e in content for gen in e[1]]
 
 
-with open('countries.json', 'w') as outfile:
+with open('places.json', 'w') as outfile:
     json.dump(content_loc, outfile, indent=2, sort_keys=True)
 
 with open('demonyms.json', 'w') as outfile:
